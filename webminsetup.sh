c92a8ed9166fa48d202c87e5c1860435a51de8b6
sirius-hollowspell <github.3d9kw@passmail.com>
2024-01-01 21:05:40 -0600

    Adjust webminsetup.sh

--- webminsetup.sh
+++ webminsetup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh | sudo sh ./setup-repos.sh
+curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
+sudo sh ./setup-repos.sh
 sudo apt-get install webmin --install-recommends
 sudo apt update && sudo apt install ufw -y && sudo ufw allow 10000
