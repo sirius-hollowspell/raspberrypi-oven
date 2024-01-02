3ab9ae320b9b619b8376a754fa63f2eb8469edc5
sirius-hollowspell <github.3d9kw@passmail.com>
2024-01-01 21:34:49 -0600

    Add -y option to webminsetup.sh

--- webminsetup.sh
+++ webminsetup.sh
@@ -2,5 +2,5 @@
 
 curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
 sudo sh ./setup-repos.sh
-sudo apt-get install webmin --install-recommends
+sudo apt-get install webmin --install-recommends -y
 sudo apt update && sudo apt install ufw -y && sudo ufw allow 10000
