22cc7a4d7f1c05682ed01737d84827fba92d6e25
sirius-hollowspell <github.3d9kw@passmail.com>
2024-01-14 12:49:06 -0600

    Add comments and -y flag to apt

--- zshsetup.sh
+++ zshsetup.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
+# Install zsh
 sudo apt update
-sudo apt install zsh
+sudo apt install zsh -y
 chsh -s $(which zsh)
 echo "Log out and lock back in to use ZSH as the default shell."
+
+# Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
