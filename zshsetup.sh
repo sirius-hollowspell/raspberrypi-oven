3c9b4c46c02d4f20476b009ede838b29c8a486e2
sirius-hollowspell <github.3d9kw@passmail.com>
2024-01-14 12:51:52 -0600

    Fix spelling error

--- zshsetup.sh
+++ zshsetup.sh
@@ -4,7 +4,7 @@
 sudo apt update
 sudo apt install zsh -y
 chsh -s $(which zsh)
-echo "Log out and lock back in to use ZSH as the default shell."
+echo "Log out and log back in to use ZSH as the default shell."
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
