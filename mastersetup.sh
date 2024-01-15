ed5e74bee5316808764b0f8332551dafcc55ed72
sirius-hollowspell <github.3d9kw@passmail.com>
2024-01-14 22:33:35 -0600

    Create mastersetup.sh

---
+++ mastersetup.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+curl https://raw.githubusercontent.com/sirius-hollowspell/raspberrypi-oven/main/casaossetup.sh | bash
+curl https://raw.githubusercontent.com/sirius-hollowspell/raspberrypi-oven/main/dockersetup.sh | bash
+curl https://raw.githubusercontent.com/sirius-hollowspell/raspberrypi-oven/main/webminsetup.sh | bash
+curl https://raw.githubusercontent.com/sirius-hollowspell/raspberrypi-oven/main/zshsetup.sh | bash
