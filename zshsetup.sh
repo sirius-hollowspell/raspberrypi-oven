4a1d2431a57dc2837eb85733c88a37316c5a14af
sirius-hollowspell <github.3d9kw@passmail.com>
2024-01-15 11:56:08 -0600

    Adjust instructions to use zsh

--- zshsetup.sh
+++ zshsetup.sh
@@ -3,10 +3,10 @@
 # Install zsh
 sudo apt update
 sudo apt install git zsh -y
-echo "Log out and log back in to use ZSH as the default shell."
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Finish the install with instructions
 echo "Run chsh -s $(which zsh) to set zsh as your default shell."
+echo "Log out and log back in to use or run zsh to use zsh."
